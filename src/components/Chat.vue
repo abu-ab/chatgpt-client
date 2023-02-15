fd1ce981b45436eb1f6bad1a531cd2d4ad90c4b7
gray <graydonl@wicrenet.com>
2023-02-15 11:24:36 +0800

    fix: 修改滚动条没有固定底部

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -1,23 +1,16 @@
 <template>
   <div class="chat-container">
     <p class="title">chat-gpt</p>
-    <div class="chat-content">
+    <div class="chat-content" ref="chatContent">
       <div class="chat-list" v-for="(item, index) of list" :key="index">
-        <div
-          class="chat-item"
-          :style="{ flexDirection: item.type ? 'row-reverse' : 'row' }"
-        >
+        <div class="chat-item" :style="{ flexDirection: item.type ? 'row-reverse' : 'row' }">
           <div class="avatar">{{ item.type ? "me" : "gpt" }}</div>
-          <div class="message-content">{{ item.message }}</div>
+          <div class="message-content" v-html="item.message"></div>
         </div>
       </div>
     </div>
     <div class="enter-box">
-      <textarea
-        class="input-box"
-        v-model="message"
-        @keyup.enter="submit"
-      ></textarea>
+      <textarea class="input-box" v-model="message" @keyup.enter="submit"></textarea>
       <button class="enter-btn" @click="submit">发送</button>
     </div>
   </div>
@@ -26,8 +19,17 @@
 <script setup>
 import axios from "axios";
 import { reactive, ref } from "@vue/reactivity";
+import { nextTick, watch } from "vue";
 const message = ref("");
 const list = reactive([]);
+const chatContent = ref(null)
+
+watch(list, () => {
+  nextTick(() => {
+    let content = chatContent.value;
+    content.scrollTop = content.scrollHeight
+  })
+})
 
 function submit() {
   if (!message.value) return;
@@ -46,6 +48,7 @@ function submit() {
     });
   });
 }
+
 </script>
     
 <style lang="less" scoped>
@@ -57,6 +60,7 @@ function submit() {
   display: flex;
   flex-direction: column;
   background: #f4f5f7;
+
   .title {
     height: 32px;
     text-align: center;
@@ -65,13 +69,45 @@ function submit() {
     font-weight: 600;
     border-bottom: 1px solid #ccc;
   }
+
   .chat-content {
     flex: 1;
     overflow: auto;
+
+    &::-webkit-scrollbar {
+      width: 10px !important;
+      /*高宽分别对应横竖滚动条的尺寸*/
+      height: 1px !important;
+    }
+
+    &::-webkit-scrollbar-thumb {
+      /*滚动条里面小方块*/
+      border-radius: 10px !important;
+      background-color: rgb(201, 201, 201) !important;
+      /*background-color: 0096c7 !important;*/
+      background-image: -webkit-linear-gradient(45deg,
+          rgba(255, 255, 255, 0.2) 25%,
+          transparent 25%,
+          transparent 50%,
+          rgba(255, 255, 255, 0.2) 50%,
+          rgba(255, 255, 255, 0.2) 75%,
+          transparent 75%,
+          transparent);
+    }
+
+    &::-webkit-scrollbar-track {
+      /*滚动条里面轨道*/
+      box-shadow: inset 0 0 5px rgba(0, 0, 0, 0.2);
+      background: #ededed;
+      border-radius: 10px;
+    }
+
+
     .chat-item {
       margin: 16px 12px 0 12px;
       display: flex;
       justify-content: flex-start;
+
       .avatar {
         width: 40px;
         height: 40px;
@@ -81,6 +117,7 @@ function submit() {
         text-align: center;
         line-height: 40px;
       }
+
       .message-content {
         line-height: 1.5;
         font-size: 14px;
@@ -95,10 +132,12 @@ function submit() {
       }
     }
   }
+
   .enter-box {
     border-top: 1px solid #ccc;
     height: 120px;
     position: relative;
+
     .input-box {
       width: 100%;
       height: 100%;
@@ -109,6 +148,7 @@ function submit() {
       font-size: 14px;
       font-weight: 100;
     }
+
     .enter-btn {
       width: 108px;
       height: 32px;
@@ -119,6 +159,7 @@ function submit() {
       right: 8px;
       bottom: 8px;
       cursor: pointer;
+
       &:hover {
         background: rgb(5, 211, 84);
         color: #fff;
